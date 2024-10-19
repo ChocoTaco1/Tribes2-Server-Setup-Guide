d1058156a2f40eac0b359f957d4c3a0638c4a477
ChocoTaco <boldfilter@zoho.com>
2024-10-19 14:54:15 -0400

    update

--- serverGuide.cs
+++ serverGuide.cs
@@ -83,7 +83,8 @@ Tacoserver: [Tacoserver Github](https://github.com/ChocoTaco1/TacoServer)
 		sudo dpkg --add-architecture i386 &&
 		sudo mkdir -pm755 /etc/apt/keyrings &&
 		sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key &&
-		sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources
+		sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources &&
+		sudo apt update
 
  - This will install wine-development, typically theres no issues.
 
